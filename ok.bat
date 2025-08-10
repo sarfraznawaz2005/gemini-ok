956e1544d1e1a7758a8496f2d0edfadb86d3a920
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2025-08-10 20:44:03 +0500

    refactor: Simplify script

--- ok.bat
+++ ok.bat
@@ -1,10 +1,2 @@
 @echo off
-
-:: Quick help -----------------------------------------------------------
-if "%~1"=="" (
-    echo Usage:  g prompt
-    echo Example: g tell me a joke
-    exit /b 1
-)
-
 gemini --yolo --prompt "%*"
